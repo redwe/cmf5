2012d6834d0a548589a6c999333e20eed67e1e58
redwe <18364120@qq.com>
2019-01-26 17:48:14 +0800

    20190126 addCourse All Data

---
+++ data/conf/route.php
@@ -0,0 +1,623 @@
+<?php	return array (
+  '监理工程师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '安全工程师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '2019 BIM/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=3',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '二级造价师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=4',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '一级造价师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=5',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '案例分析/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=6',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '进度控制/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=7',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '安全法规/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=8',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '工程管理/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=12',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'BIM应用工程师《建筑》/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=13',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'BIM应用工程师《结构》/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=14',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '科目/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=15',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '管理/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=21',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '法规/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=22',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '造价管理/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=23',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '造价案例分析/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=24',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '建设工程计价/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=25',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '技术与计量（土建）/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=27',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'BIM应用工程师《机电》/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=29',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'BIM/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=30',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '一级消防工程师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=31',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '公路水运检测师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=32',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '湖南中级工程师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=33',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '二级建造师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=34',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '一级建造师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=35',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '技术与计量（安装）/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=36',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'BIM建模工程师/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=37',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '监理工程师' => 
+  array (
+    0 => 'portal/List/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '安全工程师' => 
+  array (
+    0 => 'portal/List/index?id=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '2019 BIM' => 
+  array (
+    0 => 'portal/List/index?id=3',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '二级造价师' => 
+  array (
+    0 => 'portal/List/index?id=4',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '一级造价师' => 
+  array (
+    0 => 'portal/List/index?id=5',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '案例分析' => 
+  array (
+    0 => 'portal/List/index?id=6',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '进度控制' => 
+  array (
+    0 => 'portal/List/index?id=7',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '安全法规' => 
+  array (
+    0 => 'portal/List/index?id=8',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '工程管理' => 
+  array (
+    0 => 'portal/List/index?id=12',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'BIM应用工程师《建筑》' => 
+  array (
+    0 => 'portal/List/index?id=13',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'BIM应用工程师《结构》' => 
+  array (
+    0 => 'portal/List/index?id=14',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '科目' => 
+  array (
+    0 => 'portal/List/index?id=15',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '管理' => 
+  array (
+    0 => 'portal/List/index?id=21',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '法规' => 
+  array (
+    0 => 'portal/List/index?id=22',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '造价管理' => 
+  array (
+    0 => 'portal/List/index?id=23',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '造价案例分析' => 
+  array (
+    0 => 'portal/List/index?id=24',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '建设工程计价' => 
+  array (
+    0 => 'portal/List/index?id=25',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '技术与计量（土建）' => 
+  array (
+    0 => 'portal/List/index?id=27',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'BIM应用工程师《机电》' => 
+  array (
+    0 => 'portal/List/index?id=29',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'BIM' => 
+  array (
+    0 => 'portal/List/index?id=30',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '一级消防工程师' => 
+  array (
+    0 => 'portal/List/index?id=31',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '公路水运检测师' => 
+  array (
+    0 => 'portal/List/index?id=32',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '湖南中级工程师' => 
+  array (
+    0 => 'portal/List/index?id=33',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '二级建造师' => 
+  array (
+    0 => 'portal/List/index?id=34',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '一级建造师' => 
+  array (
+    0 => 'portal/List/index?id=35',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  '技术与计量（安装）' => 
+  array (
+    0 => 'portal/List/index?id=36',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'BIM建模工程师' => 
+  array (
+    0 => 'portal/List/index?id=37',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+);
